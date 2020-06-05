953d85a11f49b5d54683160c6a178dda375812e1
Jake Yukich <jake.yukich@gmail.com>
2020-06-05 14:18:12 -0700

    icons, nodemon, blank readme, basic app window works

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const { app, BrowserWindow } = require('electron');
 
+process.env.NODE_ENV = 'development'; // Set env
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+let mainWindow;
+
 function createMainWindow () {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'Image Shrink',
     width: 500,
-    height: 600
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev
   });
+
+  mainWindow.loadFile('./app/index.html');
 }
 
 app.on('ready', createMainWindow);
+
+/*
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+*/
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
