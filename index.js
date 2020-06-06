b8aa0ed279296cd9ca536cf452437a4e806cbe1d
Jake Yukich <jake.yukich@gmail.com>
2020-06-06 12:42:57 -0700

    log functionality added

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const slash = require('slash');
+const log = require('electron-log');
 
-process.env.NODE_ENV = 'development'; // Set env
+process.env.NODE_ENV = 'production'; // Set env
 
 const isDev = process.env.NODE_ENV !== 'production';
 const isMac = process.platform === 'darwin';
@@ -94,12 +95,12 @@ async function shrinkImage({ imgPath, quality, dest }) {
       ]
     });
 
-    console.log(files);
+    log.info(files);
     shell.openPath(dest);
 
     mainWindow.webContents.send('image:done');
   } catch (err) {
-    console.log(err);
+    log.error(err);
   }
 }
 
